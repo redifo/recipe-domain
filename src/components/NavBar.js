a5349261fbb0a5ff756505a99f310eecea2a6da0
redifo <kaanredif@gmail.com>
2024-04-17 15:51:03 +0200

    Add react router to pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import logo from '../assets/img/logo-removebg.png';
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     // Simulated authentication state
@@ -10,29 +11,31 @@ const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed='top'>
             <Container>
-                <Navbar.Brand href="#home"><img src={logo} alt="logo" height="75" />Recipe Domain</Navbar.Brand>
+                <NavLink to='/'>
+                <Navbar.Brand ><img src={logo} alt="logo" height="75" />Recipe Domain</Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto text-left">
-                        <Nav.Link href="#home"><i className="fas fa-home"></i> Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavLink className="nav-link" to='/'><i className="fas fa-home"></i> Home</NavLink>
+                        <NavLink className="nav-link" to='/Link'>Link</NavLink>
                     </Nav>
                     <Nav className='ml-auto text-left'>
                         {
                             isLoggedIn ? (
                                 <>
                                     <NavDropdown title={<span><i className="fa-solid fa-user"></i> Account</span>} id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="#profile">Profile Page</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action">Another action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#something">Something</NavDropdown.Item>
+                                        <NavDropdown.Item as={NavLink} to="/profile" >Profile Page</NavDropdown.Item>
+                                        <NavDropdown.Item as={NavLink} to="/action" >Another action</NavDropdown.Item>
+                                        <NavDropdown.Item as={NavLink} to="/something" >Something</NavDropdown.Item>
                                         <NavDropdown.Divider />
                                         <NavDropdown.Item onClick={() => setIsLoggedIn(false)}> <i class="fa-solid fa-right-from-bracket"></i> Sign Out</NavDropdown.Item>
                                     </NavDropdown>
                                 </>
                             ) : (
                                 <>
-                                    <Nav.Link className=" ms-2" ><i class="fa-solid fa-right-to-bracket"></i> Login</Nav.Link>
-                                    <Nav.Link className=" ms-2" ><i class="fa-solid fa-user-plus"></i> Sign Up</Nav.Link>
+                                    <NavLink className="nav-link ms-2" to="/login"><i class="fa-solid fa-right-to-bracket"></i> Login</NavLink>
+                                    <NavLink className="nav-link ms-2" to="/signup"><i class="fa-solid fa-user-plus"></i> Sign Up</NavLink>
                                 </>
                             )
                         }
